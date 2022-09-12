a209b53601fa67df126fb9b4cfe2b1b57731e2b0
Carlos Villegas <carlos.villegasch@gmail.com>
2022-09-12 01:39:12 -0500

    Creating Project Floating Bubble with images

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var bubbles = [];
+var img;
+
+function preload()  {
+    img = loadImage("https://upload.wikimedia.org/wikipedia/commons/3/38/Flower_July_2011-2_1_cropped.jpg");
+    //img = loadImage("images\flower1.jpg");
+}
+
+function setup() {
+    createCanvas (600, 400);
+}
+
+function mousePressed() {
+    var b = new Bubble(mouseX, mouseY);
+    bubbles.push(b);
+}
+
+function draw() {
+    background(0);
+    //image(img, 50, 50, 300, 300);
+    for (var i = bubbles.length - 1; i >= 0; i--) {
+        bubbles[i].update();
+        bubbles[i].display();
+    }
+}
