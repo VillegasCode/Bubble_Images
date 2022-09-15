35cb41464fdb4f10a05963fd75d5596d580e0ec8
Carlos Villegas <carlos.villegasch@gmail.com>
2022-09-14 20:43:41 -0500

    We've added comments to describe the code and changed the color background

--- bubble.js
+++ bubble.js
@@ -1,14 +1,19 @@
+//Constructor Function called Bubble have 3 arguments
 function Bubble(x, y, img) {
     this.x = x;
     this.y = y;
-    this.col = color(255, 100);
     this.img = img;
 
+    //"display" is a method to Bubble constructor function is used to show some flower on the canvas
     this.display = function() {
+        //This is used to create a flower from X axis and Y axis when we clicked
         imageMode(CENTER);
+        //This show the picture of a flower
         image(this.img, this.x, this.y, 50, 50);
     }
 
+    //"update" is a method to constructor function called "Bubble"
+    //Its function is to make the figures move ramdom between X axis and Y axis -1 and 1
     this.update = function() {
         this.x = this.x + random(-1, 1);
         this.y = this.y + random(-1, 1);
--- sketch.js
+++ sketch.js
@@ -1,24 +1,34 @@
+//We've created empty arrays to will be fill
 var bubbles = [];
 var flowers = [];
 
 function preload()  {
+    //We load previously the flowers images whole of LOOP FOR
     for (i = 0; i < 8; i++) {
+        //The names of pictures are correlatives
         flowers[i] = loadImage("images/flower" + i + ".png");
     }
 }
 
 function setup() {
+    //We've created canvas
     createCanvas (600, 400);
 }
 
 function mousePressed() {
+    //The "r" variable stores a random number between 0 and the length of flower array
     var r = floor(random(0, flowers.length));
+    //We've created a new variable called "b" which is a NEW INSTANCE of Bubble constructor function
+    //"b" variable has 3 arguments like mouseX, mouseY, flowers[]
     var b = new Bubble(mouseX, mouseY, flowers[r]);
+    //The PUSH method will add 3 arguments to the end of the list
     bubbles.push(b);
 }
 
 function draw() {
-    background(0);
+    //Fill the background with water color
+    background(21, 183, 224);
+    //LOOP FOR call to the methods "update" and "display" from constructor function to bubbles array each time was run
     for (var i = bubbles.length - 1; i >= 0; i--) {
         bubbles[i].update();
         bubbles[i].display();
